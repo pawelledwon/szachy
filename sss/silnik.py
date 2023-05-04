9653bdcb60a324c844c540e069379e8ed11aefe4
pawelledwon <pl300615@student.polsl.pl>
2023-05-04 12:47:15 +0200

    update

--- sss/silnik.py
+++ sss/silnik.py
@@ -15,7 +15,7 @@ class Plansza:
             [Pionek("Czarny", 1, 0, Zdjecia["cPionek"]), Pionek("Czarny", 1, 1, Zdjecia["cPionek"]), Pionek("Czarny", 1, 2, Zdjecia["cPionek"]), Pionek("Czarny", 1 ,3, Zdjecia["cPionek"]), Pionek("Czarny", 1, 4, Zdjecia["cPionek"]), Pionek("Czarny", 1, 5, Zdjecia["cPionek"]), Pionek("Czarny", 1, 6, Zdjecia["cPionek"]), Pionek("Czarny", 1, 7, Zdjecia["cPionek"])],
             [None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
+            [None, None, None, Skoczek("Czarny", 4, 3, Zdjecia["cSkoczek"]), None, None, None, None],
             [None, None, None, None, None, None, None, None],
             [Pionek("Bialy", 6, 0, Zdjecia["bPionek"]), Pionek("Bialy", 6, 1, Zdjecia["bPionek"]), Pionek("Bialy", 6, 2, Zdjecia["bPionek"]), Pionek("Bialy", 6, 3, Zdjecia["bPionek"]), Pionek("Bialy", 6, 4, Zdjecia["bPionek"]), Pionek("Bialy", 6, 5, Zdjecia["bPionek"]), Pionek("Bialy", 6, 6, Zdjecia["bPionek"]), Pionek("Bialy", 6, 7, Zdjecia["bPionek"])],
             [Wieza("Bialy", 7, 0, Zdjecia["bWieza"]), Skoczek("Bialy", 7, 1, Zdjecia["bSkoczek"]), Goniec("Bialy", 7, 2, Zdjecia["bGoniec"]), Hetman("Bialy", 7, 3, Zdjecia["bHetman"]), Krol("Bialy", 7, 4, Zdjecia["bKrol"]), Goniec("Bialy", 7, 5, Zdjecia["bGoniec"]), Skoczek("Bialy", 7, 6, Zdjecia["bSkoczek"]), Wieza("Bialy", 7, 7, Zdjecia["bWieza"])]
--- sss/skoczek.py
+++ sss/skoczek.py
@@ -1,4 +1,5 @@
 from figura import *
+from ruch import Ruch
 class Skoczek(Figura):
     nr_zdjecia = 1
     nazwa = 'Skoczek'
@@ -13,4 +14,62 @@ class Skoczek(Figura):
             ekran.blit(self.zdjecie, (37+self.kolumna*80, 65+self.rzad*80))
 
     def generuj_poprawne_ruchy(self, board):
+
+        if self.rzad > 1 and self.kolumna > 0:
+            if board[self.rzad - 2][self.kolumna - 1] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 1, self.rzad - 2), board))
+            else:
+                if board[self.rzad - 2][self.kolumna - 1].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 1, self.rzad - 2), board))
+
+        if self.rzad > 1 and self.kolumna < 7:
+            if board[self.rzad - 2][self.kolumna + 1] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 1, self.rzad - 2), board))
+            else:
+                if board[self.rzad - 2][self.kolumna + 1].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 1, self.rzad - 2), board))
+
+        if self.kolumna < 5 and self.rzad > 0:
+            if board[self.rzad - 1][self.kolumna + 2] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 2, self.rzad - 1), board))
+            else:
+                if board[self.rzad - 1][self.kolumna + 2].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 2, self.rzad - 1), board))
+
+        if self.kolumna < 5 and self.rzad < 7:
+            if board[self.rzad + 1][self.kolumna + 2] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 2, self.rzad + 1), board))
+            else:
+                if board[self.rzad + 1][self.kolumna + 2].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 2, self.rzad + 1), board))
+
+        if self.kolumna < 7 and self.rzad < 6:
+            if board[self.rzad + 2][self.kolumna + 1] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 1, self.rzad + 2), board))
+            else:
+                if board[self.rzad + 2][self.kolumna + 1].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna + 1, self.rzad + 2), board))
+
+        if self.kolumna > 0 and self.rzad < 6:
+            if board[self.rzad + 2][self.kolumna - 1] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 1, self.rzad + 2), board))
+            else:
+                if board[self.rzad + 2][self.kolumna - 1].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 1, self.rzad + 2), board))
+
+        if self.kolumna > 1 and self.rzad < 7:
+            if board[self.rzad + 1][self.kolumna - 2] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 2, self.rzad + 1), board))
+            else:
+                if board[self.rzad + 1][self.kolumna - 2].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 2, self.rzad + 1), board))
+
+        if self.kolumna > 1 and self.rzad > 0:
+            if board[self.rzad - 1][self.kolumna - 2] is None:
+                self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 2, self.rzad - 1), board))
+            else:
+                if board[self.rzad - 1][self.kolumna - 2].kolor != self.kolor:
+                    self.lista_ruchow.append(Ruch((self.kolumna, self.rzad), (self.kolumna - 2, self.rzad - 1), board))
+
+
         return self.lista_ruchow
